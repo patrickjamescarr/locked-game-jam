9a29705fe27844157f4be14cfbe2fb69f9ee41f8
Michael Walsh <michael@shootingdux.co.uk>
2021-01-11 20:53:39 +0000

    Player sprite and move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+	private void Update()
+	{
+		Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+		if (movement != Vector2.zero)
+		{
+			this.transform.Translate(new Vector3(movement.x * speed * Time.deltaTime, movement.y * speed * Time.deltaTime));
+		}
+	}
+}
