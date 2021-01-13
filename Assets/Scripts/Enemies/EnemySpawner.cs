c3b1ce12584eb383d73e2935458836e4a099d081
Michael Walsh <michael@shootingdux.co.uk>
2021-01-13 21:32:23 +0000

    Add basic spawners and placeholders

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject entityToSpawn;
+    public SpawnManagerSO spawnManager;
+    int instanceNumber;
+
+	private void Start()
+	{
+		SpawnEnemies();
+	}
+
+	private void SpawnEnemies()
+	{
+        int currentSpawnPointIndex = 0;
+
+        for (int i = 0; i < spawnManager.numberOfPrefabsToCreate; i++)
+        {
+            // Creates an instance of the prefab at the current spawn point.
+            GameObject currentEntity = Instantiate(entityToSpawn, spawnManager.spawnPoints[currentSpawnPointIndex], Quaternion.identity, this.transform);
+
+            // Sets the name of the instantiated entity to be the string defined in the ScriptableObject and then appends it with a unique number. 
+            currentEntity.name = spawnManager.prefabName + instanceNumber;
+
+            // Moves to the next spawn point index. If it goes out of range, it wraps back to the start.
+            currentSpawnPointIndex = (currentSpawnPointIndex + 1) % spawnManager.spawnPoints.Length;
+
+            instanceNumber++;
+        }
+    }
+}
