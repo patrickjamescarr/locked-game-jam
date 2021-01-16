990d991629ca867ec63c062393c7962333edb923
Michael <michael@shootingdux.co.uk>
2021-01-16 16:28:21 +0000

    Add gun to player
    Add temp sprite for gun to player prefab
    Rotate gun towards mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,8 +3,24 @@
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+	public Camera cam;
+	public Transform gun;
+
+	private void Start()
+	{
+		if (cam == null)
+		{
+			cam = Camera.main;
+		}
+	}
 
 	private void Update()
+	{
+		MovePlayer();
+		RotateGun();
+	}
+
+	private void MovePlayer()
 	{
 		Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
 
@@ -12,5 +28,20 @@ public class PlayerController : MonoBehaviour
 		{
 			this.transform.Translate(new Vector3(movement.x * speed * Time.deltaTime, movement.y * speed * Time.deltaTime));
 		}
+	}
+
+	private void RotateGun()
+	{
+		Vector2 mousePosition = Input.mousePosition;
+		Vector2 gunPosition = cam.WorldToScreenPoint(gun.position);
+
+		float angle = AngleBetweenTwoPoints(mousePosition, gunPosition);
+
+		gun.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+	}
+
+	private float AngleBetweenTwoPoints(Vector2 mousePosition, Vector2 gunPosition)
+	{
+		return Mathf.Atan2(mousePosition.y - gunPosition.y, mousePosition.x - gunPosition.x) * Mathf.Rad2Deg;
 	}
 }
---
+++ Assets/Scripts/ScriptableObjectsDefinitions/Guns/GunSO.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun", menuName = "Guns/Gun", order = 1)]
+public class GunSO : ScriptableObject
+{
+	private string gunName;
+
+	[Range(0, 100f)]
+	public float damage;
+
+	[Range(0, 5f)]
+	public float damageRange;
+
+	[Range(0, 10f)]
+	public float speed;
+
+	public GameObject bulletPrefab;
+
+	[Range(0, 10f)]
+	public float cooldownTime;
+
+	[Range(0, 10f)]
+	public float reloadTime;
+
+	[Range(0, 100)]
+	public int bulletCount;
+}
