a840c587de75236173db8e22c3d00d07c4ce3e2c
Michael <michael@shootingdux.co.uk>
2021-01-17 12:49:05 +0000

    Display pause and quit menu, display herd option and trigger from pen controller

---
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject pauseUI;
+
+    [SerializeField] private VoidEventSO quitGameEvent = default;
+
+    void OnEnable()
+    {
+        if (quitGameEvent != null)
+		{
+            quitGameEvent.OnEventRaised += QuitGame;
+		}
+    }
+
+	private void OnDisable()
+	{
+		if (quitGameEvent != null)
+		{
+            quitGameEvent.OnEventRaised -= QuitGame;
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape) && pauseUI != null)
+		{
+			bool newPause = !pauseUI.activeSelf;
+			pauseUI.SetActive(newPause);
+
+			if (newPause)
+			{
+				Time.timeScale = 0;
+			} else
+			{
+				Time.timeScale = 1;
+			}
+		}
+	}
+
+	private void QuitGame()
+	{
+        Application.Quit();
+	}
+}
---
+++ Assets/Scripts/ScriptableObjectsDefinitions/Events/VoidEventSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Events/Void Event Channel")]
+public class VoidEventSO : ScriptableObject
+{
+	public UnityAction OnEventRaised;
+
+	public void RaiseEvent()
+	{
+		OnEventRaised?.Invoke();
+	}
+}
