cb947074d8850f4115ded80ea9a71964678e1a93
Michael <michael@shootingdux.co.uk>
2021-01-17 12:01:03 +0000

    Update pen to have interact area to complete herding action

---
+++ Assets/Scripts/PenController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PenController : MonoBehaviour
+{
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Debug.Log("Player herding");
+		}
+	}
+}
