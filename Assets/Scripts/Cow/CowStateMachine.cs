bcb31f27ecb4562a9922db58fcbe37167b67cdc6
Michael <michael@shootingdux.co.uk>
2021-01-18 20:56:43 +0000

    Cow state machine - wander and follow

---
+++ Assets/Scripts/Cow/CowStateMachine.cs
@@ -0,0 +1,4 @@
+﻿public class CowStateMachine : StateMachine
+{
+	public CowStateMachine() { }
+}
---
+++ Assets/Scripts/Cow/CowWanderState.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CowWanderState : CowState
+{
+	public CowWanderState(CowController controller, StateMachine stateMachine) : base(controller, stateMachine) { }
+
+	public override void Enter()
+	{
+		CalculateNewTarget();
+	}
+
+	private void CalculateNewTarget()
+	{
+		Vector3 newTarget = new Vector3(controller.rb.position.x + Random.Range(-controller.wanderRange, controller.wanderRange), controller.rb.position.y + Random.Range(-controller.wanderRange, controller.wanderRange));
+
+		controller.UpdatePath(newTarget);
+	}
+
+	public override void LogicUpdate()
+	{
+		if (controller.mover.hasReachedEndOfPath)
+			CalculateNewTarget();
+	}
+}
